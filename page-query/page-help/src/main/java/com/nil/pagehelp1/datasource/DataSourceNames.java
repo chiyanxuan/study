55b12edf005107df376bf02cf98318b6d5739914
beccaxi <beccaxi@tencent.com>
2020-09-20 22:08:47 +0800

    数据库连接池学习汇总

--- page-query/page-help/src/main/java/com/nil/pagehelp1/datasource/DataSourceNames.java
+++ page-query/page-help/src/main/java/com/nil/pagehelp1/datasource/DataSourceNames.java
@@ -1,19 +1,8 @@
 package com.nil.pagehelp1.datasource;
 
-public interface DataSourceNames {	 
-		public static final String FIRST = "first";
-	 
-		public static final String SECOND = "second";
+public interface DataSourceNames {
 		
-		public static final String DATASOURCE1 = "datasource1";
-		 
-		public static final String DATASOURCE2 = "datasource2";
-		
-		public static final String MAPPER1 = "com.nil.pagehelp.dao";
-		 
-		public static final String MAPPER2 = "com.nil.pagehelp.dao2";
+		public static final String MAPPER1 = "com.nil.pagehelp1.dao";
 		
 		public static final String MAPPER1_PATH = "classpath:mapper/*.xml";
-
-		public static final String MAPPER2_PATH = "classpath:com/nil/mapper2/**.xml";
 }
